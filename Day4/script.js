1bbc3765e6dc4db3ce2568a07cc65c0a83bf8d8f
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-04-05 11:45:46 +0530

    Create script.js

---
+++ Day4/script.js
@@ -0,0 +1,50 @@
+// const obj ={
+//     "name": "Adarsh",
+//     "age" : 25,
+//     "city":"Bangalore"
+// }
+// obj.name =20;
+// console.log(obj); // { name: 20, age: 25, city: 'Bangalore' }
+
+// let arr = [1,4,6,8];
+// console.log(arr);
+// let res = Array.isArray(arr) ? `Yes it is an array` : `No its not an array`;
+// console.log(`Is the given object an array? ${res}`);
+
+// function checkifObject(obj) {
+//     if  (typeof obj === 'object') {
+//         return true;}
+//     else{
+//         return false;
+//     }
+// }
+// console.log(checkifObject("Adarsh"));
+// const arr = [1,2,3,4,5];
+// const obj = {
+//     "name":"Adarsh","city":"varanasi"
+// };
+
+// for(let i of arr){
+//     console.log(i);
+// }
+// for(let i of obj){
+//     console.log(i);
+// }
+
+// for(let i in arr){
+//     console.log(i);
+// }
+// for(let i in obj){
+//     console.log(i);
+// }/
+// console.log(window.document)
+// console.log(document)
+
+// console.dir(window.document)
+// console.dir(document)
+
+// const res = document.getElementsByTagName('h3')
+// console.log(res);
+
+const res = document.getElementById('ht1');
+console.log(res);

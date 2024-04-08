edfacb08924f155fce252f2167852238edfdcd6b
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-04-08 12:43:16 +0530

    Update script.js

--- Day5/Async.js/script.js
+++ Day5/Async.js/script.js
@@ -1,14 +1,37 @@
 // console.log('GEC Start');
 
-function printPretty(){
-    console.log('prettyStart');
-    let ans = 2+3;
-    console.log(ans);
-    console.log('prettyEnd');
-}
+// // function printPretty(){
+// //     console.log('prettyStart');
+// //     let ans = 2+3;
+// //     console.log(ans);
+// //     console.log('prettyEnd');
+// // }
 
-// setTimeout(printPretty,1000);
+// // // setTimeout(printPretty,1000);
 // console.log('GEC End');
 
-const btn = document.getElementById('btn');
-btn.addEventListener("click", printPretty)
+// // const btn = document.getElementById('btn');
+// // btn.addEventListener("click", printPretty)
+
+// console.log('GEC Start');
+
+//     const req = fetch("https://api.github.com/users/deepak3440");
+//     console.log(req);
+//     req.then(() => console.log("Promise is resolved"));
+// console.log('GEC End');
+
+// console.log('GEC Start');
+// fetch('https://dummyjson.com/products/1')
+// .then(res => res.json())
+// .then(json => console.log(json))
+// console.log('GEC End');
+
+// console.log('GEC Start');
+// const req = fetch('https://dummyjson.com/products/1')
+// const res = req.then(res => res.json())
+// res.then((data) => console.log(data))
+// console.log('GEC End');
+
+console.log('GEC Start');
+const req = fetch('https://dummyjson.com/products/1').then(res => res.json()).then((data) => console.log(data));
+console.log('GEC End');

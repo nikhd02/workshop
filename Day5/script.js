180a0e75a6b8b1c3e3104170e9f5f90666be9772
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-04-08 12:04:24 +0530

    Create script.js

---
+++ Day5/script.js
@@ -0,0 +1,21 @@
+// let arr = [2,18,21,82];
+
+// function printPretty(elem){
+//     console.log(': ',elem);
+// }
+//     function printArray(arr){
+//         // for (let i = 0; i < arr.length; i++) {
+//         //     printPretty(arr[i]);
+//         // }
+//         // arr.forEach(printPretty);
+
+//         arr.foreach((a) =>{ 
+//             console.log(a);
+//         });
+//         arr.map([a,b,c,d])
+//     }
+//     printArray(arr);
+
+
+
+// setTimeout(function(){console.log("Hello World")},5000);

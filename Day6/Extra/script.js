98fc849012dc63a8a95a16598fb368619f86c526
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-04-09 12:12:52 +0530

    extra

---
+++ Day6/Extra/script.js
@@ -0,0 +1,37 @@
+// const arr = [10,20,30];
+
+// const ans = arr.map((a) => {
+//     return a * 2;
+// });
+// console.log(ans); //[20,40,60]
+
+// const arr = [10,11,22,33,44,55,66];
+// console.log("Original array: ",arr);
+
+// arr.push(77);
+// console.log("\nAfter push operation :",arr);
+// const ans = arr.filter((val)=> {
+//     if(val>20)return true;
+//     else return false;
+
+// })
+
+// console.log(arr);
+// console.log(ans);
+
+// const arr = ['delhi','Mumbai','Kolkata','Pune','India','Russia','USA'];
+
+// const ans = arr.filter((val)=> {
+//         if(val.includes('i'))return true;
+//         else false;
+// });
+// console.log(`\nArray after filtering for 'i': `,ans);
+
+const arr = ['India,delhi','India,Mumbai','India,Kolkata','China,Jhong','China,Beijing','USA,New York','USA,Washington Dc'];
+
+const ans = arr.filter((val)=> {
+    const ns = val.toLowerCase();
+        if(ns.includes('india'))return true;
+        else false;
+});
+console.log(`\nArray after filtering for 'india': `,ans);
--- Day6/script.js
+++ Day6/script.js
@@ -1,7 +1,31 @@
-console.log('start');
-setTimeout(() => (console.log('....Timeout 1 .....')), 10000); // выводит "1
-console.log("start")
-setTimeout(() => (console.log('....Timeout 2 .....')), 5000);
-console.log('end');
+// // console.log('start');
+// // setTimeout(() => (console.log('....Timeout 1 .....')), 10000); // выводит "1
+// // console.log("start")
+// // setTimeout(() => (console.log('....Timeout 2 .....')), 5000);
+// // console.log('end');
 
 
+// // createorder()
+// // makepayment()
+
+// // function createorder(x){
+// //     console.log(x);
+// // }
+// // createorder(x, );
+
+// // const pr = new Promise((x, y) =>{
+// //     setTimeout(() => {x('done-123');},5000);
+// // })
+
+// // console.log(pr);
+
+// // pr.then((res)=>{
+// //     console.log(res);
+// // })
+
+// setTimeout(function abc(){
+//     console.log('Hello World!');
+// },0);
+// const pr = new Promise((res, re) =>{
+//         setTimeout(() => {x('done-123');},5000);
+//     })

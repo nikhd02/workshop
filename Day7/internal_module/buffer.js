eea591a812ccda33085b9cce0057d1cd96395cb0
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-04-10 10:41:05 +0530

    day7

---
+++ Day7/internal_module/buffer.js
@@ -0,0 +1,5 @@
+const b = new Buffer.from('Adarsh');
+
+console.log(b);
+
+console.log(b.toString());
---
+++ Day7/internal_module/script.js
@@ -0,0 +1,26 @@
+// const fs = require('node:fs');
+
+// const data = fs.readFileSync('myReadMe.txt');
+
+// console.log(data);
+
+// const data = fs.readFileSync('myReadMe.txt', {encoding: 'ucs-2'});
+
+// console.log(data);
+
+// const data = fs.readFileSync('myReadMe.txt', {encoding: 'ucs-2'});
+
+// console.log(data);
+
+// fs.writeFileSync('myReadMe.txt', 'Adarsh');
+// fs.appendFileSync('myReadMe.txt', 'Hello World');
+
+// fs.unlinkSync('myReadMe.txt');
+
+// const fs = require('fs');
+// console.log('Start');
+// const data = fs.readFileSync('./myReadMe.txt',{encoding: 'utf-8'});
+
+// console.log(data);
+// console.log('End');
+

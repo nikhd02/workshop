5e589a2167b7d412ba576994173a8ccd613525fc
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-04-16 13:30:59 +0530

    Update script.js

--- Day8/script.js
+++ Day8/script.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express()
 
-app.get('/home',(req, res) =>{
-    res.send("This is my home page");
+app.get('/api/products',(req, res) =>{
+    const data = fs.readFileSync('./data.json',"utf8");
+    const products = JSON.parse(data)
+    res.json(products);
+    // res.send("This is my home page");
 })
 
 app.listen(1400)

cd478f41414c7ddfc8cffbc2065b329cbc82f242
nikhd02 <94749949+nikhd02@users.noreply.github.com>
2024-04-18 21:57:00 +0530

    Create userControllers.js

---
+++ Amazon_backend/controllers/userControllers.js
@@ -0,0 +1,126 @@
+
+const UserModel = require('../models/userModel.js')
+
+const getAllUser = async (req, res) =>{
+    const data = await userModel.find();
+    console.log(data)
+    console.log(req.url)
+    res.json({
+        status: 'success',
+        message: 'All users',
+        data: {
+            user: data,
+        }
+    })
+}
+
+const getOneUser = async(req, res)=>{
+    try{
+
+        const data = await userModel.findOne({_id: req.params.id});
+        console.log(data)
+        console.log(req.url)
+        res.json({
+            status: 'success',
+            message: 'Find.....',
+            data: {
+                user: data,
+            }
+        })
+    }catch(err){
+        console.log(err)
+        res.json({
+            status: 'failed',
+            message: err.message
+        })
+    }
+}
+
+const addUser = async (req, res) =>{
+    try{
+
+        const data = await UserModel.create(req.body);
+        console.log(data)
+        console.log(req.body)
+        res.json({
+            status: 'success'
+        });
+    }catch(err){
+        console.log(err)
+        res.json({
+            status: 'failed',
+            message: err.message
+        });
+    }
+
+}
+
+const updateUser = async(req, res)=>{
+    try{
+        const reqId = req.params.id;
+        const data = {...req.body,_id: reqId};
+        const result = await userModel.findOneAndUpdate({_id: reqId}, data);
+        console.log(data)
+        console.log(req.body)
+        res.json({
+            status: 'success'
+        });
+    }
+    catch(err){
+        console.log(err)
+        res.json({
+            status: 'failed',
+            message: err.message
+        });
+    }
+}
+
+const replaceUser = async (req, res)=>{
+    try{
+        const reqId = req.params.id;
+        const data = {...req.body,_id: reqId};
+        const result = await userModel.findOneAndReplace({_id: reqId}, data);
+        console.log(data)
+        console.log(req.body)
+        res.json({
+            status: 'success'
+        });
+    }
+    catch(err){
+        console.log(err)
+        res.json({
+            status: 'failed',
+            message: err.message
+        });
+    }
+    
+}
+
+const deleteUser = async (req, res)=>{
+    try{
+        const reqId = req.params.id;
+        const result = await userModel.deleteOne({_id: reqId});
+        console.log(result)
+        res.json({
+            status: 'success'
+        });
+    }
+    catch(err){
+        console.log(err)
+        res.json({
+            status: 'failed',
+            message: err.message
+        });
+    }
+}
+
+
+
+module.exports = {
+    getAllUser,
+    addUser,
+    replaceUser,
+    deleteUser,
+    getOneUser,
+    updateUser
+}
